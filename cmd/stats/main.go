e7912ce46a52d9deb65c99a54b62acfef10c10ca
sauerbraten <willing.alexander@googlemail.com>
2019-03-20 22:57:38 +0100

    add basic help text

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -12,13 +12,35 @@ import (
 const gitRevision = "<filled in by CI service>"
 
 func main() {
-	// start web frontends
 	r := chi.NewRouter()
+
 	r.Use(
 		middleware.RedirectSlashes,
 		requestLogging,
 	)
+
 	r.Mount("/", NewAPI(conf.db))
+
+	r.HandleFunc("/help", func(resp http.ResponseWriter, req *http.Request) {
+		url := func(s string) string { return "http://" + req.Host + s }
+		writeln := func(s string) { resp.Write([]byte(s + "\n")) }
+
+		writeln("endpoints:")
+		writeln("- /games ?user ?mode ?map")
+		writeln("- /game/{id}")
+		writeln("- /stats ?game ?user ?mode ?map")
+		writeln("")
+		writeln("examples:")
+		writeln(url("/games"))
+		writeln(url("/games?user=pix"))
+		writeln(url("/games?mode=ectf&map=forge"))
+		writeln(url("/game/1"))
+		writeln(url("/stats"))
+		writeln(url("/stats?user=pix"))
+		writeln(url("/stats?mode=insta"))
+		writeln(url("/stats?mode=ectf&map=forge"))
+	})
+
 	log.Println("server listening on", conf.webInterfaceAddress)
 	err := http.ListenAndServe(conf.webInterfaceAddress, r)
 	if err != nil {
