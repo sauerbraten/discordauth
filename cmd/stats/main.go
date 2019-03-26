1cf6801a7a6bbc690564867ea77abc76faac8355
sauerbraten <willing.alexander@googlemail.com>
2019-03-26 16:04:51 +0100

    better /gen endpoint

--- cmd/stats/main.go
+++ cmd/stats/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -34,10 +33,26 @@ func main() {
 			return
 		}
 
-		fmt.Fprintf(resp, "add the following line to your auth.cfg:\n\n authkey \"%s\" \"%s\" \"stats.p1x.pw\"\n\n", name, hex.EncodeToString(priv))
-		fmt.Fprintf(resp, "then, copy and paste these commands and run them in-game:\n\n")
-		fmt.Fprintf(resp, " 1. /autoauth 1; connect p1x.pw\n")
-		fmt.Fprintf(resp, " 2. /servcmd register %s %s\n", name, pub.String())
+		fmt.Fprintf(resp, "+++ THIS IS ALPHA ZONE! ACCOUNTS ARE LOST AT EACH SERVER RESTART! +++")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "CREATE AN ACCOUNT IN 3 SIMPLE STEPS\n")
+		fmt.Fprintf(resp, "each step is just a command you have to execute in sauer\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "1. copy and paste the following line and execute it in in the chat prompt:\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "/authkey \"%s\" \"%s\" \"stats.p1x.pw\"; saveauthkeys; autoauth 1\n", name, priv.String())
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "2. then:\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "/connect p1x.pw\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "3. when you are connected, complete the registration:\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "/servcmd register %s %s\n", name, pub.String())
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "\n")
+		fmt.Fprintf(resp, "+++ THIS IS ALPHA ZONE! ACCOUNTS ARE LOST AT EACH SERVER RESTART! +++")
 	})
 
 	log.Println("server listening on", conf.webInterfaceAddress)
