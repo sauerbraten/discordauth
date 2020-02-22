cb4e82fc7d64bb4edbc9373a765d03fc521d935e
sauerbraten <willing.alexander@googlemail.com>
2020-02-22 17:40:39 +0100

    add pix as admin user

---
+++ migrations/3_add_pix_admin_key.down.sql
@@ -0,0 +1,1 @@
+delete from users where `name` = 'pix' and `pubkey` = '+1ae7012bf8ed0900b66b369fc953bd5252674de9d5fb86bd' and `admin` = 1;
---
+++ migrations/3_add_pix_admin_key.up.sql
@@ -0,0 +1,1 @@
+insert or ignore into `users` (`name`, `pubkey`, `admin`) values ('pix', '+1ae7012bf8ed0900b66b369fc953bd5252674de9d5fb86bd', 1);
